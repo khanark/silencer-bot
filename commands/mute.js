b9d196ea7ccf3642d463d64a87504e671851b8ea
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-05 16:29:31 +0100

    remove console.log

--- commands/mute.js
+++ commands/mute.js
@@ -13,7 +13,6 @@ module.exports = {
     if (interaction.member.roles.cache.some(role => process.env.ROLE_PERMISSIONS == role.name)) {
       const voiceChannel = await interaction.member.voice.channel;
       const currentMutes = await muteChannelMembers(voiceChannel);
-      console.log(currentMutes);
       replyMsg = currentMutes.length
         ? `Oops there are muted members ${currentMutes.length}:\n${currentMutes.join(', ')}`
         : 'Everybody is safe, 0 muted users ^^';
