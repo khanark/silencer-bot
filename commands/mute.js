925c1f16db902e2fa42ca251f905e7f08c12dfab
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-05 16:28:23 +0100

    add mute command to the bot

--- commands/mute.js
+++ commands/mute.js
@@ -1,50 +1,26 @@
 const { SlashCommandBuilder } = require('discord.js');
-const fs = require('fs/promises');
-const mutedMembers = require('../data/muted_users.json');
-
-const writeFile = async data => {
-  if (mutedMembers.some(user => user.name == data.name)) return;
-  mutedMembers.push(data);
-  await fs.writeFile('./data/muted_users.json', JSON.stringify(mutedMembers, null, 2));
-};
+const { muteChannelMembers } = require('../services/users');
+const env = require('dotenv');
+env.config();
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('mute')
-    .addStringOption(option => option.setName('channel').setDescription('select the desired channel'))
     .setDescription('Mutes the members of a selected channel if they are not muted already!'),
 
   async execute(interaction) {
-    const permitedRoles = ['Admin'];
-    const currentMutes = [];
     let replyMsg = '';
-
-    if (interaction.member.roles.cache.some(async role => permitedRoles.includes(role.name))) {
+    if (interaction.member.roles.cache.some(role => process.env.ROLE_PERMISSIONS == role.name)) {
       const voiceChannel = await interaction.member.voice.channel;
-
-      voiceChannel.members.forEach(async member => {
-        if (member.voice.mute == false) {
-          // if (!member.roles.cache.some(role => permitedRoles.includes(role.name))) {
-          const memberData = {
-            id: member.id,
-            name: member.displayName,
-          };
-          await writeFile(memberData);
-          currentMutes.push(member.displayName);
-          await member.voice.setMute(true);
-          // }
-        }
-      });
+      const currentMutes = await muteChannelMembers(voiceChannel);
+      console.log(currentMutes);
+      replyMsg = currentMutes.length
+        ? `Oops there are muted members ${currentMutes.length}:\n${currentMutes.join(', ')}`
+        : 'Everybody is safe, 0 muted users ^^';
     } else {
       replyMsg = 'No permissions';
     }
 
-    console.log(mutedMembers.length);
-    replyMsg = currentMutes.length
-      ? `Current muted members: ${currentMutes.join(', ')}\nMembers added to the channel's mutes`
-      : !replyMsg.length < 1
-      ? 'No permissions'
-      : 'Everybody is safe, 0 muted users ^^';
     await interaction.reply(replyMsg);
   },
 };
