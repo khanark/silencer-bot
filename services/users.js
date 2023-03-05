975559c8f9677aa06f0ec0e037c49f4be239bbe0
JScodeKiddie <JScodeKiddie@gmail.com>
2023-03-05 16:28:07 +0100

    split responsibility
    created user service

---
+++ services/users.js
@@ -0,0 +1,55 @@
+const fs = require('fs/promises');
+const config = require('../config.json');
+const env = require('dotenv');
+env.config();
+
+const writeFile = async file => {
+  await fs.writeFile('./config.json', JSON.stringify(file, null, 2));
+};
+
+const setNewPermissionRole = async role => {
+  if (channelMutes.botPermissionRoles.includes(role)) return;
+  channelMutes.botPermissionRoles.push(role);
+  await writeFile(channelMutes);
+};
+
+const setChannelID = async id => {
+  config.channelID = id;
+  await writeFile(config);
+};
+
+const setRoleID = async id => {
+  config.roleID = id;
+  await writeFile(config);
+};
+
+const muteChannelMembers = async channel => {
+  const currentMutes = [];
+  channel.members.forEach(async member => {
+    if (member.roles.cache.some(role => role.name == 'muted')) return;
+    if (!member.roles.cache.some(role => process.env.ROLE_PERMISSIONS == role.name)) {
+      currentMutes.push(member.displayName);
+      await member.roles.add(config.roleID);
+    }
+  });
+  return currentMutes;
+};
+
+const unmuteChannelMembers = async channel => {
+  const currentMutes = [];
+  channel.members.forEach(async member => {
+    if (member.roles.cache.some(role => role.name == 'muted')) {
+      await member.roles.remove('1081882243296940072');
+    }
+    // if (!member.roles.cache.some(role => channelMutes.botPermissionRoles.includes(role.name))) {
+    //   consosle.log(member.roles);
+    //   await member.roles.add('muted');
+    //   currentMutes.push(member.displayName);
+    //   // await member.voice.setMute(true);
+    // }
+  });
+  // await writeFile(channelMutes);
+  // return currentMutes;
+};
+
+module.exports = { setNewPermissionRole, muteChannelMembers, unmuteChannelMembers, setRoleID, setChannelID };
